73681b71359d2b76be60966e24da87aa6109f77c
Agus Tresna <89901445+agustresna@users.noreply.github.com>
2022-12-16 20:44:01 +0800

    Update Tetris.java

--- Tetris.java
+++ Tetris.java
@@ -2,7 +2,7 @@ import java.util.Random;
 import javax.swing.*;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
-import java.rmi.server.SocketSecurityException;
+
 
 public class Tetris extends JFrame implements KeyListener {
 
